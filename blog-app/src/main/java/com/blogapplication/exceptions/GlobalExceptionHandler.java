80f8c60da22d33fbd14ba85bef8cd434aa28116a
Sairaju27 <kraju9589@gmail.com>
2023-06-12 11:27:30 +0530

    Added methodargsnotFoundException but identified some things while sending the request as the messages are changing in different scenarios need to solve while not giving any values in request

--- blog-app/src/main/java/com/blogapplication/exceptions/GlobalExceptionHandler.java
+++ blog-app/src/main/java/com/blogapplication/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,18 @@
 package com.blogapplication.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.blogapplication.payloads.ApiResponse;
+
+import jakarta.el.MethodNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,5 +23,16 @@ public class GlobalExceptionHandler {
 		ApiResponse apiResponse = new ApiResponse(message,false);
 		return  new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);
 	}
-
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex){
+		Map<String, String> resp = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error)->{
+			String fieldName = ((FieldError) error).getField();
+			String message = error.getDefaultMessage();
+			resp.put(fieldName, message);
+		});
+		
+		return new ResponseEntity<Map<String,String>>(resp,HttpStatus.BAD_REQUEST);
+	}
 }
--- blog-app/src/main/java/com/blogapplication/payloads/UserDto.java
+++ blog-app/src/main/java/com/blogapplication/payloads/UserDto.java
@@ -2,6 +2,7 @@ package com.blogapplication.payloads;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,15 +16,19 @@ public class UserDto {
 	private int id;
 	
 	@NotEmpty
+	@Size(min = 10,message ="UserName should be atleast 10 characters" )
 	private String name;
 	
-	@Email
+	@Email(message="Please give correct email address")
 	private String email;
 	
 	@NotEmpty
+	@Size(min = 3,max=10,message = "Password should be atleast min of 3 and max 0f 10 charcters")
+	//@pattern used for giving regular expressions useit when required
 	private String password;
 	
 	@NotEmpty
+	@Size(min= 5,max=20,message="About should considered min of 5 and max of 20 characters")
 	private String about;
 
 }
